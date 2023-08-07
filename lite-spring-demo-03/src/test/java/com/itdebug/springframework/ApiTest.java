78d4c71b10b240e17abddb3996cd25e839f1e15b
Eric.Lu <17621698246@163.com>
2023-08-07 13:31:50 +0800

    第三节：扩展实例化bean策略：cglib或者jdk策略

--- lite-spring-demo-03/src/test/java/com/itdebug/springframework/ApiTest.java
+++ lite-spring-demo-03/src/test/java/com/itdebug/springframework/ApiTest.java
@@ -3,6 +3,10 @@ package com.itdebug.springframework;
 import com.itdebug.springframework.bean.UserService;
 import com.itdebug.springframework.entity.BeanDefinition;
 import com.itdebug.springframework.support.DefaultListableBeanFactory;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.NoOp;
 import org.junit.Test;
 
 /**
@@ -24,5 +28,50 @@ public class ApiTest {
         //3.获取Bean
         UserService userService_singleton = (UserService) beanFactory.getBean("userService", "Eric.Lu");
         System.out.println(userService_singleton.toString());
+    }
+
+    @Test
+    public void test_cglib() {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(UserService.class);
+        enhancer.setCallback(new NoOp() {
+            @Override public int hashCode() {
+                return super.hashCode();
+            }
+        });
+        Object object = enhancer.create(new Class[] {String.class}, new Object[] {"Eric.Lu"});
+        System.out.println(object);
+    }
+
+    @Test
+    public void test_jdk() throws InstantiationException, IllegalAccessException {
+        UserService userService = UserService.class.newInstance();
+        System.out.println(userService);
+        userService.getUser();
+    }
+
+    @Test
+    public void test_jdk_constructor() throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Class<UserService> userServiceClass = UserService.class;
+        Constructor<UserService> declaredConstructor = userServiceClass.getDeclaredConstructor(String.class);
+        UserService userService = declaredConstructor.newInstance("Eric.Lu");
+        System.out.println(userService);
+    }
+
+    @Test
+    public void test_parameterTypes() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<UserService> userServiceClass = UserService.class;
+        Constructor<?>[] declaredConstructors = userServiceClass.getDeclaredConstructors();
+        Constructor<?> realConstructor = null;
+        for (Constructor<?> constructor : declaredConstructors) {
+            if (constructor.getParameterTypes().length == 1) {
+                realConstructor = constructor;
+                break;
+            }
+        }
+
+        Constructor<UserService> declaredConstructor = userServiceClass.getDeclaredConstructor(realConstructor.getParameterTypes());
+        UserService userService = declaredConstructor.newInstance("Eric.Lu");
+        System.out.println(userService);
     }
 }
