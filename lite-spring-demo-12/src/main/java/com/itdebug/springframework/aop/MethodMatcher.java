91d58bcf1b8b1eeb7c7aca82151c3e4d8960327e
Eric.Lu <17621698246@163.com>
2023-08-15 23:53:25 +0800

    第12节：AspectJ 正则匹配拦截方法初体验

---
+++ lite-spring-demo-12/src/main/java/com/itdebug/springframework/aop/MethodMatcher.java
@@ -0,0 +1,14 @@
+package com.itdebug.springframework.aop;
+
+import java.lang.reflect.Method;
+
+/**
+ * @创建人 Eric.Lu
+ * @创建时间 2023/8/15
+ * @地址 https://github.com/itdebug/
+ * @描述
+ */
+public interface MethodMatcher {
+
+    boolean matches(Method method, Class<?> targetClass);
+}
---
+++ lite-spring-demo-12/src/test/java/com/itdebug/springframework/test/aop/PointcutExpressionTest.java
@@ -0,0 +1,28 @@
+package com.itdebug.springframework.test.aop;
+
+import com.itdebug.springframework.aop.aspectj.AspectJExpressionPointCut;
+import com.itdebug.springframework.test.service.SchoolService;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @创建人 Eric.Lu
+ * @创建时间 2023/8/15
+ * @地址 https://github.com/itdebug/
+ * @描述
+ */
+public class PointcutExpressionTest {
+
+    @Test
+    public void testPointcutExpression() throws Exception {
+        AspectJExpressionPointCut pointcut = new AspectJExpressionPointCut("execution(* com.itdebug.springframework.test.service.SchoolService.*(..))");
+        Class<SchoolService> clazz = SchoolService.class;
+        Method method = clazz.getDeclaredMethod("getSchool");
+
+        assertThat(pointcut.matches(clazz)).isTrue();
+        assertThat(pointcut.matches(method, clazz)).isTrue();
+    }
+}
