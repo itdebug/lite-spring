e5496dc52ab931410a03a44ce1a12179d7900a73
Eric.Lu <17621698246@163.com>
2023-08-24 13:22:18 +0800

    第14节 动态代理融入Bean生命周期

--- lite-spring-demo-15/src/main/java/com/itdebug/springframework/aop/aspectj/AspectJExpressionPointcutAdvisor.java
+++ lite-spring-demo-15/src/main/java/com/itdebug/springframework/aop/aspectj/AspectJExpressionPointcutAdvisor.java
@@ -5,6 +5,8 @@ import com.itdebug.springframework.aop.framework.PointcutAdvisor;
 import org.aopalliance.aop.Advice;
 
 /**
+ * Advisor是包含一个Pointcut和一个Advice的组合，
+ * Pointcut用于捕获JoinPoint，Advice决定在JoinPoint执行某种操作。实现了一个支持aspectj表达式的AspectJExpressionPointcutAdvisor。
  * @创建人 Eric.Lu
  * @创建时间 2023/8/23
  * @地址 https://github.com/itdebug/
